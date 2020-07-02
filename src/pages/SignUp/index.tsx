d3f04f60576e6971b87abb591f29868211a8cb1e
Saulo Medeiros <saulo.medeiros@g4flex.com.br>
2020-07-02 14:51:46 -0300

    initial commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Image,View, KeyboardAvoidingView, Platform, ScrollView} from 'react-native';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import {Container,Title, BackToSignInButton, BackToSignInButtonText} from './styles';
+
+const SignUp: React.FC = () => {
+    const navigation =  useNavigation();
+    return (
+        <>
+            <KeyboardAvoidingView style={{flex:1}} 
+            behavior={Platform.OS ==='ios' ? 'padding' : undefined}
+            enabled> 
+
+            <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{flex:1}}> 
+                <Container>
+                    <Image source={logoImg}/>
+                    <View>
+                        <Title>Crie sua conta</Title>
+                    </View>
+                     <Input name="name" icon="user" placeholder="Nome"/>
+                    <Input name="email" icon="mail" placeholder="E-mail"/>
+                    <Input name="password" icon="lock" placeholder="Senha"/>
+                    <Button>Entrar</Button>
+                </Container>
+              </ScrollView>  
+            </KeyboardAvoidingView>
+            <BackToSignInButton onPress={() => navigation.goBack()}>
+                <Icon name="arrow-left" size={20} color="#fff"/>
+                <BackToSignInButtonText>Voltar para logon</BackToSignInButtonText>
+            </BackToSignInButton>
+        </>
+    );
+}
+
+export default SignUp;
